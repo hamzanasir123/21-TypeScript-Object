83f0336e67ab48eefe5ee60b6a61bbe22d6bc2c8
Hamza Nasir <161011944+hamzanasir123@users.noreply.github.com>
2024-03-13 14:33:59 +0500

    first commit

---
+++ Typescript_object.js
@@ -0,0 +1,18 @@
+"use strict";
+// Write a program that creates Objects containing these items.
+//Person Object.
+let person = {
+    Name: 'Hamza Nasir',
+    Age: '23',
+    Nationality: 'Pakistan',
+    Student: true
+};
+console.log(person);
+//car Object.
+let car = {
+    Maker: 'Toyota',
+    Color: 'Black',
+    Automatic: true
+};
+//Print car
+console.log(car);
---
+++ Typescript_object.ts
@@ -0,0 +1,37 @@
+
+// Write a program that creates Objects containing these items.
+
+//Data Type Of Person Object.
+interface person {
+    Name : string,
+    Age : number,
+    Nationality : string,
+    Student : boolean
+}
+
+//Person Object.
+let person = {
+    Name : 'Hamza Nasir',
+    Age : '23',
+    Nationality : 'Pakistan',
+    Student : true
+}
+
+console.log(person);
+
+//Data type of car object.
+interface car {
+    Maker : string,
+    Color : string,
+    Automatice : true
+}
+
+//car Object.
+let car = {
+       Maker : 'Toyota',
+       Color : 'Black',
+       Automatic : true
+}
+//Print car
+console.log(car);
+
